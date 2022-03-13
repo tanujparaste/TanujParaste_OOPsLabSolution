e139e247627915b2b1a03bbcf01f57685cb4ed83
Tanuj Kumar Paraste <tanuj.paraste27@gmail.com>
2022-03-13 12:51:55 +0530

    added project files

---
+++ CredentialGeneratorProject/src/com/credentials/main/DriverClass.java
@@ -0,0 +1,47 @@
+package com.credentials.main;
+
+import java.util.Scanner;
+
+import com.credentials.model.Employee;
+import com.credentials.service.Credentials;
+
+public class DriverClass {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Enter employee details");
+		System.out.print("First name: ");
+		String firstName = in.nextLine();
+		System.out.print("Last name: ");
+		String lastName = in.nextLine();
+
+		Employee emp = new Employee(firstName, lastName);
+		Credentials credentials = new Credentials();
+
+		int option;
+
+		System.out.println("Please enter the department from the following" + "\n1. Technical" + "\n2. Admin"
+				+ "\n3. Human Resource" + "\n4. Legal");
+		option = in.nextInt();
+		switch (option) {
+		case 1:
+			credentials.showCredentials(emp, "Technical");
+			break;
+		case 2:
+			credentials.showCredentials(emp, "Admin");
+			break;
+		case 3:
+			credentials.showCredentials(emp, "Human Resource");
+			break;
+		case 4:
+			credentials.showCredentials(emp, "Legal");
+			break;
+		default:
+			System.out.println("Department not found.");
+			break;
+		}
+		
+		in.close();
+	}
+}
